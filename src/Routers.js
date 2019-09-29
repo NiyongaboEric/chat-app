487dbaa5e39a379b9fa50406aeea0b6caaf2e148
Eric NIYONGABO <niyongaboeric69@gmail.com>
2019-09-29 16:09:14 +0200

    ft(path): tweek path

--- src/Routers.js
+++ src/Routers.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Signup from './components/User/Signup/Signup';
-import Signin from './components/User/Signin/Signin';
+import Signup from './Components/User/Signup/Signup';
+import Signin from './Components/User/Signin/Signin';
 import './index.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routers from './routers';
+import Routers from '../src/routers';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Routers />, document.getElementById('root'));
