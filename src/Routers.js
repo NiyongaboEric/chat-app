b792b74d39e893a2ff2ae19184bd5085c058e838
Eric NIYONGABO <niyongaboeric69@gmail.com>
2019-09-29 13:00:05 +0200

    ft(request): add signin route

--- src/Routers.js
+++ src/Routers.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Signup from './components/User/Signup/Signup.js';
+import Signin from './components/User/Signin/Signin';
 import './index.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 
@@ -10,6 +11,7 @@ function Routers() {
       <div className="container">
         <Switch>
           <Route path="/signup" exact component={Signup} />
+          <Route path="/signin" exact component={Signin} />
         </Switch>
       </div>
     </Router>
