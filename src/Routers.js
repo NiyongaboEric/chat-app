73cd0b26da9b5b101579f6da3fb74c9fcad00d9a
Eric NIYONGABO <niyongaboeric69@gmail.com>
2019-09-29 14:34:21 +0200

    ft(bg): in router edit path too call signup to render

--- src/Routers.js
+++ src/Routers.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Signup from './components/User/Signup/Signup.js';
+import Signup from './components/User/Signup/Signup';
 import Signin from './components/User/Signin/Signin';
 import './index.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
