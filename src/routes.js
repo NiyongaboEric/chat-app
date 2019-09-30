6b3e8ee176c8c64b3d15d2f7b1cf39f705319039
Eric NIYONGABO <niyongaboeric69@gmail.com>
2019-09-30 17:23:00 +0200

    ft(path): lower case component

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './index.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
-import UserSignup from './components/User/Signup/Signup.js';
-import Signin from './components/User/Signin/Signin.js';
+import Signup from './components/user/signup/signup.js';
+import Signin from './components/user/signin/signin.js';
 
 
 const Routers = () => {
@@ -11,7 +11,7 @@ const Routers = () => {
     <Router>
       <div className="container">
         <Switch>
-          <Route path="/signup" exact component={UserSignup} />
+          <Route path="/signup" exact component={Signup} />
           <Route path="/signin" exact component={Signin} />
         </Switch>
       </div>
