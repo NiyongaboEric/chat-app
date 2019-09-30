f883796d79c1e674f45f43db3b398f0f0c3a2e45
Eric NIYONGABO <niyongaboeric69@gmail.com>
2019-09-30 12:21:59 +0200

    ft(routes): tweek routes path

--- src/Routers/Routes.js
+++ src/Routers/Routes.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import UserSignup from '../Components/User/Signup/Signup.js';
-import Signin from '../Components/User/Signin/Signin.js';
 import '../index.css';
 import '../../node_modules/font-awesome/css/font-awesome.min.css';
+const UserSignup = require('../Components/User/Signup/Signup');
+const UserSignin = require('../Components/User/Signin/Signin');
 
 const Routers = () => {
   return (
@@ -11,7 +11,7 @@ const Routers = () => {
       <div className="container">
         <Switch>
           <Route path="/signup" exact component={UserSignup} />
-          <Route path="/signin" exact component={Signin} />
+          <Route path="/signin" exact component={UserSignin} />
         </Switch>
       </div>
     </Router>
