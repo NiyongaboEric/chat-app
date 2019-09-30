d2bf44105f25eff5f57ad3491b77edee0021c0d5
Eric NIYONGABO <niyongaboeric69@gmail.com>
2019-09-30 16:47:27 +0200

    ch(routes): import signup in routes

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './index.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
-import Signup from './components/User/Signup/Signup.js';
+import UserSignup from './components/User/Signup/Signup.js';
 import Signin from './components/User/Signin/Signin.js';
 
 
@@ -11,7 +11,7 @@ const Routers = () => {
     <Router>
       <div className="container">
         <Switch>
-          <Route path="/signup" exact component={Signup} />
+          <Route path="/signup" exact component={UserSignup} />
           <Route path="/signin" exact component={Signin} />
         </Switch>
       </div>
