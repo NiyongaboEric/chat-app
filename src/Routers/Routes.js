baebd2c8df61081492758bc5e4a3fd9af63e0385
Eric NIYONGABO <niyongaboeric69@gmail.com>
2019-09-30 13:14:23 +0200

    ft(route): change order of import

--- src/Routers/Routes.js
+++ src/Routers/Routes.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import '../index.css';
 import '../../node_modules/font-awesome/css/font-awesome.min.css';
-const UserSignup = require('../Components/User/Signup/Signup');
-const UserSignin = require('../Components/User/Signin/Signin');
+import Signup from '../Components/User/Signup/Signup';
+import Signin from '../Components/User/Signin/Signin';
 
-const Routers = () => {
+function Routers() {
   return (
     <Router>
       <div className="container">
         <Switch>
-          <Route path="/signup" exact component={UserSignup} />
-          <Route path="/signin" exact component={UserSignin} />
+          <Route path="/signup" exact component={Signup} />
+          <Route path="/signin" exact component={Signin} />
         </Switch>
       </div>
     </Router>
