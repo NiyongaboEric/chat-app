baffe79313d7e3684f8fb464a0f9ed17dfe6158c
Eric NIYONGABO <niyongaboeric69@gmail.com>
2019-09-30 11:35:05 +0200

    ft(routes): edit function in routes

--- src/Routers/Routes.js
+++ src/Routers/Routes.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Signup from '../Components/User/Signup/Signup.js';
+import UserSignup from '../Components/User/Signup/Signup.js';
 import Signin from '../Components/User/Signin/Signin.js';
 import '../index.css';
 import '../../node_modules/font-awesome/css/font-awesome.min.css';
 
-function Routers() {
+const Routers = () => {
   return (
     <Router>
       <div className="container">
         <Switch>
-          <Route path="/signup" exact component={Signup} />
+          <Route path="/signup" exact component={UserSignup} />
           <Route path="/signin" exact component={Signin} />
         </Switch>
       </div>
