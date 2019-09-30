19f96777d9d78e806a9a15d5ba9e5f64dd7a7981
Eric NIYONGABO <niyongaboeric69@gmail.com>
2019-09-30 17:28:04 +0200

    ft(path): use class name of component in route

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './index.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
-import Signup from './components/user/signup/signup.js';
+import UserSignup from './components/user/signup/signup.js';
 import Signin from './components/user/signin/signin.js';
 
 
@@ -11,7 +11,7 @@ const Routers = () => {
     <Router>
       <div className="container">
         <Switch>
-          <Route path="/signup" exact component={Signup} />
+          <Route path="/signup" exact component={UserSignup} />
           <Route path="/signin" exact component={Signin} />
         </Switch>
       </div>
