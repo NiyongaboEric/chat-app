806c6845ffbed51944b7451246e7603c77ecf651
NiyongaboEric <33821734+NiyongaboEric@users.noreply.github.com>
2019-10-01 09:04:50 +0200

    Ft user signup 168514868 (#2)
    * ft(signup): add icons [#168514868]
    
    * ft(signup-route): combine all components [#168514868]
    
    * ft(step-one): signup inital step[#168514868]
    
    * ft(request): add http request packages
    
    * ft(signup): design signup ui [#168514868]
    
    * ch(signup): finish expected signup ui [#168514868]
    
    * ch(cleanup): separate ui and backend packages [#168514868]
    
    * ft(request): add signin route
    
    * ft(validation): edit fullnames in singular
    
    * ft(improove):finish signup ui
    
    * ft(bg): in router edit path too call signup to render
    
    * ft(debug): add packages and edit routes
    
    * ft(path): tweek path
    
    * ft(path): change routes paths
    
    * ft(signup): add extensions in routes
    
    * ft(signup): edit path in routes
    
    * ft(routes): edit function in routes
    
    * ft(routes): tweek routes path
    
    * ft(route): change order of import
    
    * ft(route):change to es6 arrow function
    
    * ft(route): solve error routes
    
    * ft(package): add more rules in package.json
    
    * ft(route): import components with extension
    
    * ch(routes): import signup in routes
    
    * ft(path): lower case component
    
    * ft(path): use class name of component in route

---
+++ src/components/User/Signup/PersonCredentials.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../../Images/logo.jpg';
+import './Signup.css';
+import Advertise from '../Advertise/Advert';
+
+
+class PersonCredentials extends Component {
+
+  render() {
+    const { handleChange, nextStep, values, error } = this.props;
+    return (
+      <div className="signup">
+        <Advertise />
+        <div className="signup-content">
+          <div className="logo">
+            <img src={logo} alt="Free Online Logo Maker by DesignEvo free logo creator" />
+          </div>
+          <div className="signup-details">
+            <h2 className="motto">Talk to everyone without borders</h2>
+            <span className="signin-link">Already signed up?
+              <Link to="/signin"> Login</Link>
+            </span>
+          </div>
+          <div className="signup-form">
+            <form>
+              <div className="form-input">
+                <label>Full names</label>
+                <input
+                  type="text"
+                  name="fullname"
+                  value={values.fullname}
+                  onChange={handleChange}
+                  placeholder="&#xf007; Shamim Kelai"
+                  required
+                />
+              </div>
+              {error.fullname && <div className="input-error">{error.fullname}</div>}
+              <div className="form-input">
+                <label>Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  placeholder="&#xf0e0; shamim@kelai.ex"
+                  required
+                />
+              </div>
+              {error.email && <div className="input-error">{error.email}</div>}
+              <div className="form-input">
+                <label>Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  value={values.password}
+                  onChange={handleChange}
+                  placeholder="&#xf023; *****"
+                  required
+                />
+              </div>
+              {error.password && <div className="input-error">{error.password}</div>}
+              <div className="form-input">
+                <button
+                  onClick={nextStep}
+                  type="button"
+                >
+                  Save and Continue
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  };
+}
+
+export default PersonCredentials;
---
+++ src/routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './index.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+import UserSignup from './components/user/signup/signup.js';
+import Signin from './components/user/signin/signin.js';
+
+
+const Routers = () => {
+  return (
+    <Router>
+      <div className="container">
+        <Switch>
+          <Route path="/signup" exact component={UserSignup} />
+          <Route path="/signin" exact component={Signin} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default Routers;
