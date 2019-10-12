7c6ed25f13fdb80fe010c4e1a47f99721829a89c
Eric NIYONGABO <niyo@Erics-MacBook-Pro-4.local>
2019-10-12 10:21:32 +0200

    ft(refactore): use es6 javascript

--- src/Redux/actions/signin.js
+++ src/Redux/actions/signin.js
@@ -17,36 +17,34 @@ export const signinError = (message) => {
 }
 
 export const signinApi = ({ email, password }) => {
-  return (dispatch) => {
+  return async (dispatch) => {
     const url = 'https://chat-app-edition-api.herokuapp.com/api/signin';
-    axios
-      .post(
+    try {
+      const result = await axios.post(
         url, {
-          email,
+          email, 
           password
         }, {
           headers: {
             "Content-Type": "application/json"
           }
         }
-      )
-      .then(res => {
-        if (res.status === 200) {
-          const { token } = res.data.data;
-          const { message, status } = res.data;
-          localStorage.setItem('token', token);
-          dispatch(signin({ message, status }));
-        }
-      })
-      .catch(err => {
-        if (err.response) {
-          const { message } = err.response.data;
-          return dispatch(signinError({ message }));
-        }
-        if (err.request) {
-          return dispatch(signinError({ message: 'Network Error' }));
-        }
-        return dispatch(signinError({ message: 'Server Error' }));
-      })
+      );
+      if (result.status === 200) {
+        const { data, message, status } = result.data;
+        const { token } = data;
+        localStorage.setItem('token', token);
+        dispatch(signin({ message, status }));
+      }
+    } catch(err) {
+      if (err.response) {
+        const { message } = err.response.data;
+        return dispatch(signinError({ message }));
+      }
+      if (err.request) {
+        return dispatch(signinError({ message: 'Network Error' }));
+      }
+      return dispatch(signinError({ message: 'Server Error' }));
+    }
   }
 };
